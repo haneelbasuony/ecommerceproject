4604715eb5088e0c33262038dcd21b9c9afe7190
Hane Hatem <hanehatemelbasuony20@gmail.com>
2025-02-16 16:23:27 +0200

    fix payment

--- src/Apis/payment.js
+++ src/Apis/payment.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 let token = localStorage.getItem('token');
 export function paymentOnline({ cartId, shippingAddress }) {
   return axios.post(
-    `https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartId}?url=http://https://haneelbasuony.github.io/ecommerceproject`,
+    `https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartId}?url=https://haneelbasuony.github.io/home`,
     { shippingAddress },
     { headers: { token } }
   );
