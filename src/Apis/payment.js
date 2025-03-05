33eb6e97c6ba4545efe42899ef696b65fe0e5443
Hane Hatem <hanehatemelbasuony20@gmail.com>
2025-03-05 02:25:52 +0200

    fixed cart

--- src/Apis/payment.js
+++ src/Apis/payment.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-let token = localStorage.getItem('token');
 export function paymentOnline({ cartId, shippingAddress }) {
+  let token = localStorage.getItem('token');
+
   return axios.post(
     `https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartId}?url=https://ecommerceproject-ten.vercel.app/`,
     { shippingAddress },
